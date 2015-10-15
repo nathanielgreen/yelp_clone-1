02f0690cab1d5ec32d00ce68aa4024c5005aa643
nathanielgreen <ngreen2303@gmail.com>
2015-10-15 15:44:14 +0100

    Users can now only leave one review per restaurant. All tests passing

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 feature 'reviewing' do
-	before {Restaurant.create name: 'KFC'}
+  before do
+    sign_up
+    add_place
+  end
 
 	scenario 'allows users to leave a review using a form' do
 		visit '/restaurants'
@@ -13,6 +16,18 @@ feature 'reviewing' do
 		expect(current_path).to eq '/restaurants'
 		expect(page).to have_content('so so')
 	end
+  scenario 'user cannot leave more than one review' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "bad"
+    select '2', from: 'Rating'
+    click_button 'Leave Review'
+    expect(page).not_to have_content('bad')
+  end
 end
 
 describe 'reviews' do
@@ -32,4 +47,22 @@ describe 'reviews' do
       expect(review.user).to eq user
     end
   end
+end
+
+
+
+def sign_up
+	visit('/')
+	click_link('Sign up')
+	fill_in('Email', with: 'test@example.com')
+	fill_in('Password', with: 'testtest')
+	fill_in('Password confirmation', with: 'testtest')
+	click_button('Sign up')
+end
+
+def add_place
+  visit('/restaurants')
+  click_link('Add a restaurant')
+  fill_in('Name', with: 'KFC')
+  click_button('Create Restaurant')
 end
